6437c7c91b3723d8458b3bc6324cf3cff263ac06
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 18:29:03 +0700

    notFoundMiddleware

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Server running...");
+import express from "express";
+const app = express();
+
+// middleware
+notFoundMiddleware;
+import notFoundMiddleware from "./middleware/not-found.js";
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT | 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
