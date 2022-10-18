d1d74aea04701f0b207396ca9f5d0ee3a9c4c259
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 14:31:29 +0700

    Alert component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Logo, FormRow } from "../components";
+import { Logo, FormRow, Alert } from "../components";
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
+  showAlert: false,
 };
 
 const Register = () => {
@@ -27,6 +28,7 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
+        {values.showAlert && <Alert />}
         {/* name input */}
         <FormRow
           type="text"
