79d7db4fbbee34011202bb523821b859ffa66871
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 23:56:43 +0700

    connect to DB

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
 
 app.get("/", (req, res) => {
-  throw new Error("error");
   res.send("Welcome!");
 });
 
@@ -16,6 +16,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
