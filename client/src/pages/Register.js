36886b8484793d6d158407a3421f61972bcbc738
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 13:54:49 +0700

    register page - setup

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,84 @@
-const Register = () => {
-  return <h1>Register</h1>;
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
 };
+
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name input */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
 
 export default Register;
