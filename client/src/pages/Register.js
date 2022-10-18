bfbc0452fff7b6b8c9b5a10b0e315006ec4ccc3c
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 14:15:49 +0700

    FormRow component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Logo } from "../components";
+import { Logo, FormRow } from "../components";
 
 const initialState = {
   name: "",
@@ -28,18 +28,26 @@ const Register = () => {
         <Logo />
         <h3>Login</h3>
         {/* name input */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            value={values.name}
-            name="name"
-            onChange={handleChange}
-            className="form-input"
-          />
-        </div>
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type="submit" className="btn btn-block">
           Submit
         </button>
