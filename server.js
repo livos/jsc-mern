73bf2c0a46de0f4ae68909552436305ccb3cf1b1
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 23:33:54 +0700

    .env and gitignore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
