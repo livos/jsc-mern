c99b34785683a955801c4c31d138359f2eb146bc
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-18 23:14:40 +0700

    errorHandlerMiddleware

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from "express";
 const app = express();
 
-// middleware
-notFoundMiddleware;
 import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
+  throw new Error("error");
   res.send("Welcome!");
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT | 5000;
 
