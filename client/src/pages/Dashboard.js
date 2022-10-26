a56030e46be504241ff6dc3ca0f43978215e7f6b
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-26 15:37:51 +0700

    Register User - setup

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,6 @@
 import { useEffect } from "react";
 
 const Dashboard = () => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("http:///localhost:5000");
-      // const response = await fetch("/data.json");
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  });
   return <h1>Dashboard</h1>;
 };
 
