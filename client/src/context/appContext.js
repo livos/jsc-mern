1c4f6687e88454375c4ac7b6a5f327b730a806fd
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-26 17:07:31 +0700

    programmatically navigate to dashboard

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -44,15 +44,15 @@ const AppProvider = ({ children }) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
       const response = await axios.post("/api/v1/auth/register", currentUser);
-      console.log(response);
+      // console.log(response);
       const { user, token, location } = response.data;
       dispatch({
         type: REGISTER_USER_SUCCESS,
         payload: { user, token, location },
       });
-      // kicak storage kater
+      // kicak storage later
     } catch (error) {
-      console.log(error.response);
+      // console.log(error.response);
       dispatch({
         type: REGISTER_USER_ERROR,
         payload: { msg: error.response.data.msg },
