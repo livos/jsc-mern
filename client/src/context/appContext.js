41871ff8b9a7ff879632d5a762ecace929eef39b
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-27 19:51:24 +0700

    toggle sidebar

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -12,6 +12,7 @@ import {
   LOGIN_USER_BEGIN,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -27,6 +28,7 @@ const initialState = {
   token: token,
   userLocation: userLocation || "",
   jobLocation: userLocation || "",
+  showSidebar: false,
 };
 
 const AppContext = React.createContext();
@@ -100,9 +102,13 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const toggleSidebar = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
+
   return (
     <AppContext.Provider
-      value={{ ...state, displayAlert, registerUser, loginUser }}
+      value={{ ...state, displayAlert, registerUser, loginUser, toggleSidebar }}
     >
       {children}
     </AppContext.Provider>
