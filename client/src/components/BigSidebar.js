6849ae0e3bd650aaf1475fa44e65f3787bb35165
livos <lvivancos2nd-dev@yahoo.fr>
2022-10-28 17:07:34 +0700

    big sidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>BigSidebar</div>;
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
