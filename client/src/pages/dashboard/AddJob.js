dfe12f318f7076ea910989d1b8fa8c45d57f9ec2
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-04 15:56:46 +0700

    add job page - structure complete

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,143 @@
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import styled from "styled-components";
+
 const AddJob = () => {
-  return <h1>Add Job Page</h1>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext();
+
+  const hanleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+    console.log("create job");
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* location */}
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* job type */}
+          {/* job status */}
+          <div className="btn-container">
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={hanleSubmit}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default AddJob;
+
+const Wrapper = styled.section`
+  border-radius: var(--borderRadius);
+  width: 100%;
+  background: var(--white);
+  padding: 3rem 2rem 4rem;
+  box-shadow: var(--shadow-2);
+  h3 {
+    margin-top: 0;
+  }
+  .form {
+    margin: 0;
+    border-radius: 0;
+    box-shadow: none;
+    padding: 0;
+    max-width: 100%;
+    width: 100%;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    row-gap: 0.5rem;
+  }
+  .form-center button {
+    align-self: end;
+    height: 35px;
+    margin-top: 1rem;
+  }
+  .btn-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+    align-self: flex-end;
+    margin-top: 0.5rem;
+    button {
+      height: 35px;
+    }
+  }
+  .clear-btn {
+    background: var(--grey-500);
+  }
+  .clear-btn:hover {
+    background: var(--black);
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      column-gap: 1rem;
+    }
+    .btn-container {
+      margin-top: 0;
+    }
+  }
+  @media (min-width: 1120px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    .form-center button {
+      margin-top: 0;
+    }
+  }
+`;
