c64bbe4719de1b13555871f4d5d6f6237c2c7e12
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-07 15:48:14 +0700

    get all jobs - state values

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -46,6 +46,10 @@ const initialState = {
   jobType: "full-time",
   statusOptions: ["interview", "declined", "pending"],
   status: "pending",
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
 };
 
 const AppContext = React.createContext();
