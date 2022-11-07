5792ef97d7781f8e21839bace5396c702184f592
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-07 21:11:46 +0700

    job component

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -239,6 +239,14 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const setEditJob = (id) => {
+    console.log(`set edit job ${id}`);
+  };
+
+  const deleteJob = (id) => {
+    console.log(`delete job ${id}`);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -253,6 +261,8 @@ const AppProvider = ({ children }) => {
         clearValues,
         createJob,
         getJobs,
+        setEditJob,
+        deleteJob,
       }}
     >
       {children}
