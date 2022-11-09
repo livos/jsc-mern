01d55c360a6ecce346cd19efe6002afe08da492d
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-09 17:11:32 +0700

    populate db

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -4,7 +4,7 @@ import NavLinks from "./NavLinks";
 import Logo from "../components/Logo";
 
 const BigSidebar = () => {
-  const { showSidebar, toggleSidebar } = useAppContext();
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
       <div
@@ -16,7 +16,7 @@ const BigSidebar = () => {
           <header>
             <Logo />
           </header>
-          <NavLinks toggleSidebar={toggleSidebar} />
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
