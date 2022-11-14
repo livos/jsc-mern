9005ca6422d2470223f57edf29908caf8107c7de
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-14 16:25:09 +0700

    query params - job type

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -21,7 +21,24 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { status, jobType, sort, search } = req.query;
+
+  const queryObject = { createdBy: req.user.userId };
+
+  // add stuff based on condition
+  if (status !== "all") {
+    queryObject.status = status;
+  }
+
+  if (jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  let result = Job.find(queryObject);
+
+  // chain sor conditions
+  const jobs = await result;
+
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
