25325b7dbc594bb0cfc9e6d1e886769e14862569
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-15 15:05:12 +0700

    search component - complete

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -20,18 +20,53 @@ const SearchContainer = () => {
     if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
   return (
     <Wrapper>
       <form className="form">
         <h4>search form</h4>
         <div className="form-center">
+          {/* search position */}
           <FormRow
             type="text"
             name="search"
             value={search}
             handleChange={handleSearch}
           />
-          {/* rest of the inputs */}
+          {/* search by status */}
+          <FormRowSelect
+            lagelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={["all", ...statusOptions]}
+          />
+          {/* search by type */}
+          <FormRowSelect
+            lagelText="type"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={["all", ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disables={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
         </div>
       </form>
     </Wrapper>
