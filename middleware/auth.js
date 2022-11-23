e0e20395743b968537d7f567cbadf68eaa9af1cd
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-23 21:01:03 +0700

    test user - restrict access

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,8 +10,8 @@ const auth = async (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId };
-
+    const testUser = payload.userId === "637c9b16f8e4e658e8c2e874";
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
     throw new UnAuthenticatedError("Authentication invalid");
