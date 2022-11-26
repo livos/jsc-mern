16ba9270b9334c17b2d40eadcda891f6b0691b41
livos <lvivancos2nd-dev@yahoo.fr>
2022-11-26 20:07:21 +0700

    test cookie expiration

--- utils/attachCookies.js
+++ utils/attachCookies.js
@@ -2,7 +2,7 @@ const attachCookies = ({ res, token }) => {
   const oneDay = 1000 * 60 * 60 * 24;
   res.cookie("token", token, {
     httpOnly: true,
-    expires: new Date(Date.now() + 100000),
+    expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === "production",
   });
 };
